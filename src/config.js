da3cd68c9abb28ce84f03606bb193336c3bed1c4
Justin Lin <wancat@wancat.cc>
2022-08-04 10:37:10 +0800

    :recycle: Move to root folder, Add dotenv

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
-export const { TOKEN } = process.env;
+import 'dotenv/config';
 
-export const CLIENT_ID = '968159446381522966';
-export const GUILD_ID = '763001015712350231';
-export const TARGET_CHANNEL_ID = '970941286112714832';
+export const { TOKEN, CLIENT_ID } = process.env;
+
+export const GUILD_ID = '475663520428720140';
+export const TARGET_CHANNEL_ID = '475669759300468737';
 export const COMMAND_NAME = 'translate';
 export const COMMAND_OPTION_NAME = 'address';
 
