c7c5535065b212230029d660f60fc6016ba11438
Justin Lin <wancat@wancat.cc>
2022-08-05 10:55:58 +0800

    :sparkles: Add issue ISCN link

--- src/commands/depub.js
+++ src/commands/depub.js
@@ -1,5 +1,6 @@
 import { ContextMenuCommandBuilder } from '@discordjs/builders';
 import { ApplicationCommandType } from 'discord-api-types/v10';
+import { URL, URLSearchParams } from 'url';
 
 export default {
   data: new ContextMenuCommandBuilder()
@@ -9,6 +10,18 @@ export default {
     const msg = await interaction.channel.messages.fetch(interaction.targetId);
     console.log('msg:', msg);
     const msgUrl = `https://discordapp.com/channels/${msg.guildId}/${msg.channelId}/${msg.id}`;
-    await interaction.reply(`${msg.content} ${msgUrl}`);
+    const msgContent = msg.content.substring(0, 2048);
+    const iscnUrl = new URL('/in/widget/iscn', 'https://testnet.like.co');
+    iscnUrl.search = new URLSearchParams({
+      title: `depub.space-${(new Date()).toISOString()}`,
+      url: msgUrl,
+      fingerprint: 'https://depub.blog',
+      type: 'Record',
+      tags: `Discord,${interaction.member.guild.name}`,
+      publisher: 'depub.space',
+      description: `${msgUrl}\n${msgContent}`,
+    });
+
+    await interaction.reply(`Click this link to continue: ${iscnUrl.toString()}`);
   },
 };
