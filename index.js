7ac2d81e8be8be7507764e2cbbbfeed747cebf0c
William Chong <me@williamchong.cloud>
2022-09-23 04:59:01 +0800

    🔧 Use UI_BASE_PATH as api base

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import { initDB } from './backend/src/db.js';
-import { TOKEN, BACKEND_PORT, BACKEND_HOST } from './backend/src/config.js';
+import {
+  TOKEN, UI_BASE_PATH, BACKEND_PORT, BACKEND_HOST
+} from './backend/src/config.js';
 import client from './backend/src/client.js';
 import router from './backend/src/router.js';
 
 initDB();
 const app = express();
-app.use('/discord', router);
-app.use('/discord', express.static('frontend/dist'));
+app.use(UI_BASE_PATH, router);
+app.use(UI_BASE_PATH, express.static('frontend/dist'));
 app.listen(BACKEND_PORT, BACKEND_HOST);
 console.log(`Server listening on port ${BACKEND_HOST}:${BACKEND_PORT}`);
 client.login(TOKEN);
