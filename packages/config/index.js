95ee271ed210a1ff7de2fc864dd20f37b49ac231
William Chong <me@williamchong.cloud>
2022-09-23 05:04:20 +0800

    🐛 Remove tailing / in UI_URL

--- packages/config/index.js
+++ packages/config/index.js
@@ -7,7 +7,7 @@ export const SEND_GRANT_EXPIRATION = 30 // 30 days
 export const SESSION_EXPIRATION = 15 // 15 minutes
 export const UI_BASE_PATH = IS_DEV ? '/' : '/discord/';
 const UI_BASE = process.env.UI_BASE || IS_DEV ? 'http://localhost:3000' : `https://api.${IS_TESTNET ? 'rinkeby.' : 'like.co'}`
-export const UI_URL = `${UI_BASE}${UI_BASE_PATH}`;
+export const UI_URL = `${UI_BASE}${UI_BASE_PATH}`.replace(/\/+$/, '');
 export const BACKEND_HOST = process.env.BACKEND_HOST || '127.0.0.1';
 export const BACKEND_PORT = process.env.BACKEND_PORT || 8000;
 export const BACKEND_URL = process.env.BACKEND_URL || `http://${BACKEND_HOST}:${BACKEND_PORT}`
@@ -25,34 +25,34 @@ export const CHAIN_EXPLORER = IS_TESTNET
 
 export const WALLET_CONFIG = IS_TESTNET
   ? {
-      chainId: 'likecoin-public-testnet-5',
-      chainName: 'LikeCoin',
-      rpcURL: 'https://node.testnet.like.co/rpc/',
-      restURL: 'https://node.testnet.like.co',
-      coinType: 118,
-      coinDenom: 'EKIL',
-      coinMinimalDenom: 'nanoekil',
-      coinDecimals: 9,
-      bech32PrefixAccAddr: 'like',
-      bech32PrefixAccPub: 'likepub',
-      bech32PrefixValAddr: 'likevaloper',
-      bech32PrefixValPub: 'likevaloperpub',
-      bech32PrefixConsAddr: 'likevalcons',
-      bech32PrefixConsPub: 'likevalconspub',
-    }
+    chainId: 'likecoin-public-testnet-5',
+    chainName: 'LikeCoin',
+    rpcURL: 'https://node.testnet.like.co/rpc/',
+    restURL: 'https://node.testnet.like.co',
+    coinType: 118,
+    coinDenom: 'EKIL',
+    coinMinimalDenom: 'nanoekil',
+    coinDecimals: 9,
+    bech32PrefixAccAddr: 'like',
+    bech32PrefixAccPub: 'likepub',
+    bech32PrefixValAddr: 'likevaloper',
+    bech32PrefixValPub: 'likevaloperpub',
+    bech32PrefixConsAddr: 'likevalcons',
+    bech32PrefixConsPub: 'likevalconspub',
+  }
   : {
-      chainId: 'likecoin-mainnet-2',
-      chainName: 'LikeCoin',
-      rpcURL: 'https://mainnet-node.like.co/rpc/',
-      restURL: 'https://mainnet-node.like.co',
-      coinType: 118,
-      coinDenom: 'LIKE',
-      coinMinimalDenom: 'nanolike',
-      coinDecimals: 9,
-      bech32PrefixAccAddr: 'like',
-      bech32PrefixAccPub: 'likepub',
-      bech32PrefixValAddr: 'likevaloper',
-      bech32PrefixValPub: 'likevaloperpub',
-      bech32PrefixConsAddr: 'likevalcons',
-      bech32PrefixConsPub: 'likevalconspub',
-    }
+    chainId: 'likecoin-mainnet-2',
+    chainName: 'LikeCoin',
+    rpcURL: 'https://mainnet-node.like.co/rpc/',
+    restURL: 'https://mainnet-node.like.co',
+    coinType: 118,
+    coinDenom: 'LIKE',
+    coinMinimalDenom: 'nanolike',
+    coinDecimals: 9,
+    bech32PrefixAccAddr: 'like',
+    bech32PrefixAccPub: 'likepub',
+    bech32PrefixValAddr: 'likevaloper',
+    bech32PrefixValPub: 'likevaloperpub',
+    bech32PrefixConsAddr: 'likevalcons',
+    bech32PrefixConsPub: 'likevalconspub',
+  }
