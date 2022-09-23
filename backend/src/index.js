eedc220e1c07b0dbbfdda6129cd58960e1444b93
William Chong <me@williamchong.cloud>
2022-09-24 00:57:01 +0800

    🔒️ Add hardcoded csp

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,33 @@
 import express from 'express';
 import helmet from 'helmet';
 import { initDB } from './db.js';
-import { TOKEN, BACKEND_PORT, BACKEND_HOST } from './config.js';
+import {
+  TOKEN,
+  BACKEND_PORT,
+  BACKEND_HOST,
+  LIKECOIN_CHAIN_ENDPOINT,
+} from './config.js';
 import client from './client.js';
 import router from './router.js';
 
 initDB();
 const app = express();
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      'script-src': [
+        "'self'",
+        'sha256-tempUn1btibnrWwQxEk37lMGV1Nf8FO/GXxNhLEsPdg=',
+        'sha256-7cJdwMd4UlYhTrkYbWHiWKgRMr1cbk0aAnatJSAIM1s=',
+      ],
+      'connect-src': [
+        "'self'",
+        'data:',
+        LIKECOIN_CHAIN_ENDPOINT,
+      ],
+    },
+  },
+}));
 app.use(router);
 app.listen(BACKEND_PORT, BACKEND_HOST);
 client.login(TOKEN);
--- index.js
+++ index.js
@@ -2,14 +2,34 @@ import express from 'express';
 import helmet from 'helmet';
 import { initDB } from './backend/src/db.js';
 import {
-  TOKEN, UI_BASE_PATH, BACKEND_PORT, BACKEND_HOST
+  TOKEN,
+  UI_BASE_PATH,
+  BACKEND_PORT,
+  BACKEND_HOST,
+  LIKECOIN_CHAIN_ENDPOINT,
 } from './backend/src/config.js';
 import client from './backend/src/client.js';
 import router from './backend/src/router.js';
 
 initDB();
 const app = express();
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      'script-src': [
+        "'self'",
+        'sha256-tempUn1btibnrWwQxEk37lMGV1Nf8FO/GXxNhLEsPdg=',
+        'sha256-7cJdwMd4UlYhTrkYbWHiWKgRMr1cbk0aAnatJSAIM1s=',
+      ],
+      'connect-src': [
+        "'self'",
+        'data:',
+        LIKECOIN_CHAIN_ENDPOINT,
+      ],
+    },
+  },
+}));
+
 app.use(UI_BASE_PATH, router);
 app.use(UI_BASE_PATH, express.static('frontend/dist'));
 app.listen(BACKEND_PORT, BACKEND_HOST);
